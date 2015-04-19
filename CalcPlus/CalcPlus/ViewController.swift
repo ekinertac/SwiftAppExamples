f1faad3f8212194bc90dc08c45ddef4500620331
ekinertac <ekinertac@gmail.com>
2015-04-19 03:41:26 +0300

    adding comments

--- CalcPlus/CalcPlus/ViewController.swift
+++ CalcPlus/CalcPlus/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  CalcPlus
 //
-//  Created by Ekin Ertac on 18/04/15.
-//  Copyright (c) 2015 Ekin Ertac. All rights reserved.
+//  Created by ekinertac on 18/04/15.
+//  Copyright (c) 2015 ekinertac. All rights reserved.
 //
 
 import UIKit
@@ -11,17 +11,20 @@ import UIKit
 class ViewController: UIViewController {
     // MARK: Outlets
     @IBOutlet weak var resultLabel: UILabel!
-    
+
     // MARK: Actions
     @IBAction func clearButtonDidTouch(sender: AnyObject) {
+        // Clear text from resultLabel view
         resultLabel.text = ""
     }
-    
+
     @IBAction func evaluateButtonDidTouch(sender: AnyObject) {
+        // Call evaulate func
         evaulateString()
     }
-    
+
     @IBAction func backspaceButtonDidTouch(sender: AnyObject) {
+        // perform backspace event
         resultLabel.text = deleteLastCharacter(resultLabel.text!)
     }
 
@@ -32,7 +35,7 @@ class ViewController: UIViewController {
         background.frame = self.view.bounds
         // Add gradient to view
         self.view.layer.insertSublayer(background, atIndex: 0)
-        
+
         for item in self.view.subviews {
             if item.isKindOfClass(UIButton) && item.tag == 1 {
                 // Set target for number buttons
@@ -43,26 +46,30 @@ class ViewController: UIViewController {
     }
 
     func numbersAndOperandsDidTouch(sender: UIButton!) {
+        // Add button labels to resultLabel
         let value = sender.titleLabel!.text!
         resultLabel.text = resultLabel.text! + value
     }
 
     func evaulateString() {
+        // Evaulte math string
         let stringWithMathOperation = resultLabel.text!
         let expressionEngine = NSExpression(format: stringWithMathOperation)
         let calculatedResult: AnyObject = expressionEngine.expressionValueWithObject(nil, context: nil)
         resultLabel.text = "\(calculatedResult)"
+        // Safe to use eval in Swift <3
     }
-    
+
     func deleteLastCharacter(sendStr: String) -> String {
-        var str: String = sendStr
-        var result: String = ""
+        var str: String = sendStr // clone!
+        var result: String = "" // return statement
         let stringLength = count(str)
         if stringLength > 0 {
             let substringIndex = stringLength - 1
             result = str.substringToIndex(advance(str.startIndex, substringIndex))
+            // remove (totalCount - 1)nd char from string
         }
         return result
     }
-    
+
 }
